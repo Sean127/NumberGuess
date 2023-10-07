c425877dd94f085a48162d4c040d714b54b947b6
Sean127 <sdoran401@gmail.com>
2023-10-07 11:19:25 +0000

    Add random module and function to generate random number

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+''' Needed to obtain random number '''
+import random
+
+MAX_GUESS = 3
+
+
+def get_random_number():
+    ''' Generates random number '''
+    number = random.randint(1, 10)
+    return number
